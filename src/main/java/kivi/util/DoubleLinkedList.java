a5147da2a6d065dcd4c049da8edb7fc9e1fdf58b
danii <daniiltsemin@gmail.com>
2025-03-14 14:05:31 +0300

    refactor

--- src/main/java/kivi/util/DoubleLinkedList.java
+++ src/main/java/kivi/util/DoubleLinkedList.java
@@ -3,6 +3,9 @@ package kivi.util;
 import kivi.Node;
 
 public class DoubleLinkedList {
+    private Node head;
+    private Node tail;
+
     public void printInReverse() {
         Node head = null;
         Node current = head;
@@ -17,6 +20,22 @@ public class DoubleLinkedList {
         System.out.println("NULL");
     }
 
+    public boolean checkIsPalindrome() {
+        Node head = null;
+        Node current = head;
+        while (current.getNext() != null) {
+            current = current.getNext();
+        }
+        while (current != null) {
+            if (current.getData() != current.prev.getData()) {
+                return false;
+            }
+        }
+        return true;
+    }
 
-
+    @Override
+    public String toString() {
+        return "DoubleLinkedList{}";
+    }
 }
